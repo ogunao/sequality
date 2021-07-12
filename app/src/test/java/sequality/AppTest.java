2696125ec85ffff0ba7b407f289525f220f23e17
ogunao <m1m21a13@oit.ac.jp>
2021-07-12 10:17:31 +0900

    add gradle project2

--- app/src/test/java/sequality/AppTest.java
+++ app/src/test/java/sequality/AppTest.java
@@ -11,4 +11,9 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+    @Test
+public void testAppReternHelloWorld() {
+App classUnderTest = new App();
+assertEquals("Hello World!", classUnderTest.getGreeting());
+}
 }
